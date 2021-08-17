b74df489236f4e9f217cb4a89ab954bc7ee4d5a7
Yijia-Xiao <xyj_thu@163.com>
2021-08-17 20:58:06 +0800

    update some scripts

--- pretrain/msa_tools/prepare_msa.sh
+++ pretrain/msa_tools/prepare_msa.sh
@@ -1,33 +1,13 @@
-NAME=UniRef50-xa-a2m-2017
-python prepare_msa.py /dataset/f0a0efb9/protein/data/$NAME > $NAME.json
-
-cp -rp  $NAME.json /workspace/
-
-exit
-
-PFAM=./
-VOCAB=./
+NAME=UniRef50-xa-a2m-2017-large
 
 mkdir -p /workspace/msa/$NAME
-cp ${NAME}.json /workspace/msa/$NAME/
+python prepare_msa.py /dataset/f0a0efb9/protein/data/UniRef50-xa-a2m-2017 > /workspace/msa/$NAME/$NAME.json
+# python prepare_msa.py /dataset/f0a0efb9/protein/data/$NAME > $NAME.json
+
+#PFAM=./
+#VOCAB=./
 
 python ../tools/preprocess_data.py --input /workspace/msa/$NAME/$NAME.json \
-	--tokenizer-type BertWordPieceCase --vocab-file $VOCAB/iupac_vocab.txt \
+	--tokenizer-type BertWordPieceCase --vocab-file ./iupac_vocab.txt \
 	--output-prefix /workspace/msa/$NAME/$NAME --dataset-impl mmap --workers 96
 
-
-
-# NAME=tiny
-# python prepare_msa.py /dataset/f0a0efb9/protein/msa/data/tiny > $NAME.json
-
-
-# PFAM=./
-# VOCAB=./
-
-# python ../tools/preprocess_data.py --input $PFAM/$NAME.json \
-# 	--tokenizer-type BertWordPieceCase --vocab-file $VOCAB/iupac_vocab.txt \
-# 	--output-prefix $PFAM/$NAME --dataset-impl mmap --workers 128
-
-# mkdir -p /workspace/msa/$NAME
-# cp ${NAME}_text_document.bin /workspace/msa/$NAME/
-# cp ${NAME}_text_document.idx /workspace/msa/$NAME/
